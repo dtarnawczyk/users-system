ffd4b7e7280f03578395e0710dbb1f460ca10d54
Dominik Tarnawczyk <dtarnawczyk@gmail.com>
2017-06-05 22:21:12 +0200

    Internationalization introduced

---
+++ src/main/java/org/customers/system/web/config/WebConfiguration.java
@@ -0,0 +1,8 @@
+package org.customers.system.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+}
