9bf2517842f2682a57605d21e90938f1acb1ed92
Dominik Tarnawczyk <dtarnawczyk@gmail.com>
2017-07-14 11:15:50 +0200

    Contextpath set and html src fixed

--- src/main/java/org/customers/system/web/config/WebConfiguration.java
+++ src/main/java/org/customers/system/web/config/WebConfiguration.java
@@ -6,24 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableConfigurationProperties(PictureProperties.class)
-@EnableSwagger2
+//@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public Docket customerApi(){
-        return new Docket(DocumentationType.SWAGGER_2).select().paths(path -> {
-            assert path != null;
-            return path.startsWith("/api/");
-        }).build();
-    }
+//    @Bean
+//    public Docket customerApi(){
+//        return new Docket(DocumentationType.SWAGGER_2).select().paths(path -> {
+//            assert path != null;
+//            return path.startsWith("/api/");
+//        }).build();
+//    }
 }
